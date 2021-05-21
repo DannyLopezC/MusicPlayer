37784834b12821a164e5741e614bea51c14c514a
Danny López Cárdenas <70038382+DannyLopezC@users.noreply.github.com>
2021-05-21 05:32:19 -0500

    Initial commit

---
+++ Assets/Scripts/AudioManager.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.UI;
+
+namespace MusicPlayer
+{
+    [RequireComponent(typeof(AudioSource))]
+    public class AudioManager : MonoBehaviour
+    {
+        public List<Playlist> playLists;
+        public AudioSource source;
+
+        private void Start()
+        {
+            source = GetComponent<AudioSource>();
+            source.clip = playLists[0].musicList[0].audioClip;
+            //Play();
+        }
+
+        public void Play()
+        {
+            Debug.Log($"play");
+            if (source.clip == null) return;
+            else
+                Debug.Log($"Null song");
+
+            if (!source.isPlaying)
+                source.Play();
+            else
+                source.Pause();
+        }
+
+        public void Stop()
+        {
+
+        }
+    }
+}
---
+++ Assets/Scripts/Playlist.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Sirenix.OdinInspector;
+
+namespace MusicPlayer
+{
+    [System.Serializable, CreateAssetMenu(fileName = "Playlist_", menuName = "MusicPlayer/Playlists")]
+    public class Playlist : ScriptableObject
+    {
+        [System.Serializable]
+        public class Song
+        {
+            public AudioClip audioClip;
+            public int id;
+            public string songName;
+        }
+
+        public string playListName;
+        public int id;
+        public List<Song> musicList;
+
+        [Button]
+        public void SetValues()
+        {
+            for (int i = 0; i < musicList.Count; i++)
+            {
+                musicList[i].songName = musicList[i].audioClip.name;
+                musicList[i].id = i;
+            }
+        }
+    }
+}
